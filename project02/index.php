db323100efe604336a87793e31fa12402b0568a8
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-02-26 11:16:05 +0900

    push project 02

---
+++ project02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head> 
+        <meta charset="UTF-8">
+        <meta http-equiv="X-AU-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, inintial-scale=1.0">
+        <title>Project02</title>
+    </head>
+    <body>
+
+        <h1>
+            <?php echo "Hello Project O2"; ?>
+        </h1>
+
+        <p>
+            <?php echo "Welcome to 'Project 02'..."; ?>
+        </p>
+
+    </body>
+    </html>
