0975e25f0bfee94640e336fbf9a01f1f33e04547
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-02-26 11:32:05 +0900

    push project03

---
+++ project03/comments.php
@@ -0,0 +1,19 @@
+<?php
+    // this is my first single line in comment php//
+    echo "single line comment using '//' <br></br> ";
+
+    #this is also a 
+    #single line comment
+    echo "single line comment using '#' <br></br> ";
+
+    /*
+
+    this is
+     
+    multipile line comment 
+     
+    in php
+     
+     */
+    echo"multipile line comment in php using '/*...*/'"
+?>
